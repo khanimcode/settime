d30b3157c19119fd82ad694e630c514afa0f90cd
khanimcode <7f3p8e2@code.edu.az>
2022-12-05 13:11:27 +0400

    created

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+// const date=document.querySelector(".form-time")
+// const givenDate=document.querySelector(".form-control")
+// const btn=document.querySelector(".btn")
+// btn.addEventListener("click",()=>{
+//     const date = new Date();
+// const currentDate=date.getDate()
+// if(givenDate.value>currentDate){
+//     console.log("dfvbfnn");
+// }
+
+// })
+
+// const span1 = document.querySelector("#span1");
+// const span2 = document.querySelector("#span2");
+// const span3 = document.querySelector("#span3");
+// const stopbtn=document.querySelector(".stopbtn")
+// const myfunc = () => {
+//   const date = new Date();
+//   span1.innerHTML = `${moment(date).format("MMMM Do YYYY, h:mm:ss a")}`;
+
+//   var newyorkDate = new Date(
+//     new Date().toLocaleString("en-US", { timeZone: "America/New_York" })
+//   );
+//   span2.innerHTML = `${moment(newyorkDate).format("MMMM Do YYYY, h:mm:ss a")}`;
+// };
+// const timer = setInterval(myfunc,1000);
+// stopbtn.addEventListener("click",()=>{
+//     clearInterval(timer);
+// })
+
+//TASK3
